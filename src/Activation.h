ec8911465398e8f6a9be1f7aa2a0fea834fdf956
GonewithGt <1724532024@qq.com>
2017-07-18 02:11:20 -0700

    添加tanh激活函数及测试程序

--- src/Activation.h
+++ src/Activation.h
@@ -152,7 +152,72 @@ namespace redtea {
             static shared_ptr<Softmax> create(PTensor in) {
                 return shared_ptr<Softmax>(new Softmax(in));
             }
-        };    
+        };
+		
+        class Tanh : public Tensor {
+        public :
+            Tanh() : Tensor() {}
+            Tanh(PTensor in) : Tensor() {
+                inputs.push_back(in);
+            }
+            Tanh(Tensor& in) : Tensor() {
+                inputs.push_back(in.copy());
+            }
+        public :
+                Tanh(const Tanh& other) {
+                    set(other);
+                }
+                 
+                typedef Tanh Type;
+                shared_ptr<Tensor> copy() const{
+                    shared_ptr<Type> c(new Type());
+                    c->setParam(this->getParam());
+                    c->setInputs(this->getInputs());
+                    c->setOptimizer(this->getOptimizer());
+                    return c;
+                }
+
+        public :
+            void forward() {
+                Tensor::forward();
+
+                MatrixX& in = inputs[0]->getOutput();
+                MatrixX& o = this->getOutput();
+                o.resize(in.rows(), in.cols());
+     
+                for(int i=0;i<in.rows();i++) {
+                    for(int j=0;j<in.cols();j++) {
+                    
+					o(i, j) = (1.0 - exp(-in(i, j)*2.0))/(1.0 + exp(-in(i, j)*2.0));
+					
+                    }
+                }
+            }
+
+            void backward(const MatrixX& deltaLoss) {
+                this->addLoss(deltaLoss);
+
+                MatrixX& o = this->getOutput();
+                const MatrixX& l = deltaLoss;
+
+                MatrixX deltaLossIn = MatrixX::Zero(o.rows(), o.cols()); 
+                for(int i=0;i<o.rows();i++) {
+                    for(int j=0;j<o.cols();j++) {
+                        
+						deltaLossIn(i, j) = 1.0 - o(i, j) * o(i, j);
+                        deltaLossIn(i, j) *= l(i, j);
+                    }
+                }
+                inputs[0]->backward(deltaLossIn);    
+            }
+
+        public :
+            static shared_ptr<Tanh> create(PTensor in) {
+                return shared_ptr<Tanh>(new Tanh(in));
+            }
+        };
+
+		
     };
 };
 #endif
